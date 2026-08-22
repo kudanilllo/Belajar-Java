819f8e9beed7856bece16c79cf5e9afa1978c2c8
kudanilllo <akhtarfauzan9@gmail.com>
2026-08-22 12:47:28 +0800

    Delete JAVA LEARN directory

--- JAVA LEARN/main.java
+++
@@ -1,6 +0,0 @@
-public class main {
-public static void main (String[]args){
-    String nama = "Akhtar Fauzan";
-    System.out.print("Hallo " + nama);}
-
-}
--- JAVA LEARN/partone.java
+++
@@ -1,9 +0,0 @@
-public class partone {
-    //ini adalahkode java pertama ku
-    
-    public static void main(String[] args){
-        System.out.print(500 * 2);
-
-        
-    }
-}
